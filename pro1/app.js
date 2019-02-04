83b37ecaafbb48f14bf434bb461caec0e0c21d30
Anurag1153 <32287160+Anurag824@users.noreply.github.com>
2019-02-04 21:22:55 +0530

    Add files via upload

--- pro1/app.js
+++ pro1/app.js
@@ -1,11 +1,41 @@
 var btns=document.querySelectorAll('#movie-list .delete');
-btns.forEach(function(btn){
+	btns.forEach(function(btn){
 	btn.addEventListener('click', function(e){
 	const li=e.target.parentElement;
 	li.parentNode.removeChild(li);
 	});
-});
+	});
+function add(){
+	var a=document.querySelectorAll('#add-movie input');
+	var node=document.createElement("li");
+	var b=document.createElement('span');
+	b.setAttribute("class","name");
+	var na=document.createTextNode(a[1].value);
+	b.appendChild(na);
+	node.append(b);
+	var c=document.createElement('span');
+	c.setAttribute("class","delete");
+	var del=document.createTextNode('delete');
+	c.appendChild(del);
+	node.append(c);
+	document.getElementsByTagName("ul")[0].appendChild(node);
+
+	var btns=document.querySelectorAll('#movie-list .delete');
+	btns.forEach(function(btn){
+	btn.addEventListener('click', function(e){
+	const li=e.target.parentElement;
+	li.parentNode.removeChild(li);
+	});
+	});
+}
 /*
+function add(){
+	var a=document.querySelectorAll('#add-movie input');
+	var node = document.createElement("LI");
+  	var textnode = document.createTextNode(a[1].value);
+  	node.appendChild(textnode);
+  	document.getElementByTagName("ul").appendChild(node);
+}
 arrays
 
 title
